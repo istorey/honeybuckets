fbe5fcd10f64cc563483b7b79e5cd06e1e69aaff
Ausin Formica <austin.formica@gmail.com>
2015-01-26 15:37:16 -0500

    sign in works but not saving username

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,8 @@ class SessionsController < ApplicationController
     # raise request.env["omniauth.auth"].to_yaml
     auth = request.env['omniauth.auth']
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+    session[:user_id] = user.id
+    redirect_to root_url, :notice => "Signed in!"
      
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
   has_many :reviews
+
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.provider = auth["provider"]
+      user.uid = auth["uid"]
+      user.name = auth["name"]
+    end
+  end
 end
