82bd8ecf5b2fb819fa3e6e7a85ceb1c31ca272f9
JackCurtis <jack.v.curtis@gmail.com>
2015-01-26 13:58:11 -0500

    create table migrations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,4 @@
 class Review < ActiveRecord::Base
+  belongs_to :location
+  belongs_to :user
 end
--- db/migrate/20150126181149_create_reviews.rb
+++ db/migrate/20150126181149_create_reviews.rb
@@ -1,6 +1,11 @@
 class CreateReviews < ActiveRecord::Migration
   def change
     create_table :reviews do |t|
+      t.integer :rating
+      t.string :body
+      t.string :image_url
+      t.references :user
+      t.references :location
     end
   end
 end
