efb0a4865a7ec1f08d39b76dd5413e6b4613c6b9
Ausin Formica <austin.formica@gmail.com>
2015-01-26 15:16:19 -0500

    update session controller for omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,10 @@
 class SessionsController < ApplicationController
 
   def create
-    raise request.env["omniauth.auth"].to_yaml  
+    # raise request.env["omniauth.auth"].to_yaml
+    auth = request.env['omniauth.auth']
+    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+     
   end
 
 end
