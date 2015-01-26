b7927df1418043215ca67d1629c787184d6ff56b
Ausin Formica <austin.formica@gmail.com>
2015-01-26 15:11:35 -0500

    update users db with uid & provider columns

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150126181149) do
+ActiveRecord::Schema.define(version: 20150126201037) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 20150126181149) do
     t.string "name"
     t.string "email"
     t.string "password_digest"
+    t.string "provider"
+    t.string "uid"
   end
 
 end
