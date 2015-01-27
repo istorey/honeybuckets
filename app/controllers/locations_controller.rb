ef866843320cf02a0a9ff3c13c038b8a9efded25
istorey <wrightstoreys@gmail.com>
2015-01-27 09:53:43 -0500

    geocoder implemetted no db

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,14 +3,21 @@ class LocationsController < ApplicationController
   end
 
   def map
+  if params[:search].present?
+    @locations = Location.near(params[:search], 50, :order => :distance)
+  else
+    @locations = Location.all
+  end
   end
 
   def show
+    @location = Location.new(lat:0, long:0)
   end
 
   def new
   end
 
   def create
+    
   end
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,9 +1,9 @@
 class Location < ActiveRecord::Base
   has_many :reviews
-  reverse_geocoded_by :lat, :long
-  	:adress => :location
+  reverse_geocoded_by :lat, :long,
+  	:address => :location
   after_validation :reverse_geocode
   
-  geocoded_by :address
-  after_validation :geocode
+  geocoded_by :address, :latitude => :lat, :longitude => :long
+  # after_validation :geocode
 end
