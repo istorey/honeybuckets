0658e475d5bbb9e3c98a4ec752b736118b8e35f1
JackCurtis <jack.v.curtis@gmail.com>
2015-01-28 10:36:38 -0500

    pull address from lat/long on save

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,7 +2,7 @@ class Location < ActiveRecord::Base
   has_many :reviews
   reverse_geocoded_by :lat, :long,
   	:address => :location
-  #after_validation :reverse_geocode
+  # after_validation :reverse_geocode
   
   geocoded_by :address, :latitude => :lat, :longitude => :long
   # after_validation :geocode
